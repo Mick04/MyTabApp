5f84ffa5d08d26a5a92b6fde477a6b65a597f104
Mick <mac5y4@talktalk.net>
2024-03-20 11:53:45 +0000

    commit befor ejecting from expo

--- Screen1.js
+++ Screen1.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import App from './App'
+// import App from './App'
 
 export default function Sceen1() {
     return (
